be4b0d747d2cc9b4e226a03dd1c6057eee4c5ea1
Arlan Marreiro  (BR-MIS) <bb0ga139@cal-comp.com.br>
2021-11-10 14:57:02 -0400

    Integração JSX com JavaScript

--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -1,9 +1,24 @@
-export default function Home(){    
+export default function Home() {
     // return '<h1>Home</h1>'
+    // const a = 1
+    // const b = 2    
+    // console.log(a * b)
+
+    // const obj = {nome:'Arlan Marreiro', idade: 27}
+
+    const titulo = <h1>Fundamentos de Next.JS & React</h1>
+
+    function subtitulo() {
+        return <h2>Estudando os conceitos do Framework</h2>
+    }
     return (
-        <div>
-            <h1>Fundamentos de Next.JS & React</h1>
-            <h2>Estudando os conceitos do Framework</h2>
+        <div>            
+            {titulo}
+            {subtitulo()}            
+            <h2>{" Teste de Maiusculo".toUpperCase()}</h2>
+            <p>
+                {JSON.stringify({nome:'Arlan Marreiro', idade: 27})}
+            </p>
         </div>
     )
 }
