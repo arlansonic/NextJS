70bc8818c754e894e52c448931a63af5e21408af
Arlan Marreiro  (BR-MIS) <bb0ga139@cal-comp.com.br>
2021-11-19 10:48:53 -0400

    Component Properties

--- fundamentos/src/components/Cabecalho.jsx
+++ fundamentos/src/components/Cabecalho.jsx
@@ -1,6 +1,7 @@
-export default function Cabecalho() {
+export default function Cabecalho(props) {
     return (
-        <h1>Fundamentos com Next.JS</h1>
+        <header>
+            <h1>{props.titulo}</h1>
+        </header>
     )
-
 }
