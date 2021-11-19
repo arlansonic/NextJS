47672ace426c9a4d364da7d34b29f9a07ea50601
Arlan Marreiro  (BR-MIS) <bb0ga139@cal-comp.com.br>
2021-11-19 11:27:39 -0400

    Navigating Between Components

--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -1,24 +1,10 @@
+import Link from 'next/link'
 export default function Home() {
-    // return '<h1>Home</h1>'
-    // const a = 1
-    // const b = 2    
-    // console.log(a * b)
-
-    // const obj = {nome:'Arlan Marreiro', idade: 27}
-
-    const titulo = <h1>Fundamentos de Next.JS & React</h1>
-
-    function subtitulo() {
-        return <h2>Estudando os conceitos do Framework</h2>
-    }
     return (
-        <div>            
-            {titulo}
-            {subtitulo()}            
-            <h2>{" Teste de Maiusculo".toUpperCase()}</h2>
-            <p>
-                {JSON.stringify({nome:'Arlan Marreiro', idade: 27})}
-            </p>
+        <div>
+            <Link href="/estilo">
+                Estiloso
+            </Link>
         </div>
     )
 }
