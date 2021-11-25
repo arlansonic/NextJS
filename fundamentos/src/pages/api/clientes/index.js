86404c75eac5d0575c94e2183e242db5c539d0da
Arlan Marreiro  (BR-MIS) <bb0ga139@cal-comp.com.br>
2021-11-25 12:11:46 -0400

    Api com NextJS

---
+++ fundamentos/src/pages/api/clientes/index.js
@@ -0,0 +1,18 @@
+import handler from "../hello"
+
+export default function cliente(req, res) {
+
+    if (req.method === "GET") {
+        clienteGet(req, res)
+    } else {
+        res.status(405).send('Não encontrado')
+    }
+}
+
+function clienteGet(req, res) {
+    res.status(200).json({
+        "id": 1,
+        "nome": "Arlan Marreiro",
+        "idade": 3
+    })
+}
--- fundamentos/src/pages/api/hello.js
+++ fundamentos/src/pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-    res.status(200).json({ name: 'Arlan Marreiro' })
+    res.status(200).json({
+        name: 'Arlan Marreiro',
+        metodo: req.method
+    })
 }
