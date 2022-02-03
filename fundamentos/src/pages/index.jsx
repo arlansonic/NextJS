c7cf506699978bf7a33da3c7305a273157506872
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-02-03 14:48:23 -0400

    Update Comentario

--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -15,7 +15,9 @@ export default function Home() {
             <Navegador texto="Navegação #01" destino="/navegacao" cor="green"/>
             <Navegador texto="Navegação #02" destino="/cliente/AM/123"/>
             <Navegador texto="Componente com Estado" destino="/estado" cor="purple"/>            
-            <Navegador texto="Integração" destino="/integracao_1" cor="red"/>            
+            <Navegador texto="Integração" destino="/integracao_1" cor="red"/>
+            <Navegador texto="Conteúdo Estático" destino="/estatico" cor="#6dd5ed"/>
+            
         </div>
     )
 }
